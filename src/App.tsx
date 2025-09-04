7bea3329afcfc19d5dbe1682c12c3a33996bfa2b
Ahmedatia99 <atyaa629@gmail.com>
2025-09-04 05:15:48 +0300

    setup route react dom to contributors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+// import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+// import Navbar from "./components/common/Navbar";
+import Footer from "./components/common/Footer";
+
 function App() {
-  return <>hello world</>;
+  return (
+    <>
+      {/* <Navbar /> */}
+      <Routes>
+        {/* Put your routes here Like that */}
+        
+        {/* <Route path="/" element={<Home />} /> */}
+      </Routes>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
