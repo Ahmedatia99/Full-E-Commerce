33710f74d2b1543b8afb889e33c5598cb2e22e3e
Ahmedatia99 <atyaa629@gmail.com>
2025-09-08 22:21:37 +0300

    add button as reusable components with footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,37 @@
 import { Routes, Route } from "react-router-dom";
 // import Navbar from "./components/common/Navbar";
 import Footer from "./components/common/Footer";
-
+import { Button } from "./components/ui/button";
 function App() {
   return (
     <>
       {/* <Navbar /> */}
-      <Routes>
-        {/* Put your routes here Like that */}
-        
-        {/* <Route path="/" element={<Home />} /> */}
-      </Routes>
+      {/*  */}
       <Footer />
+      <div className="p-10 flex gap-5">
+        <Button
+          variant="default"
+          size="lg"
+          onClick={() => alert("Button clicked default!")}
+        >
+          click
+        </Button>
+        <Button
+          variant="secondary"
+          size="lg"
+          onClick={() => alert("Button clicked secondary!")}
+        >
+          click
+        </Button>
+        <Button
+          variant="small"
+          size="lg"
+          onClick={() => alert("Button clicked small!")}
+          className=" "
+        >
+          click
+        </Button>
+      </div>
     </>
   );
 }
