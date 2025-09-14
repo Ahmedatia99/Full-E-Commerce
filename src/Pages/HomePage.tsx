c8fc83991cffe809cef2b9b9414cf3758f1cf8ab
unknown <bamrmagdy@gmail.com>
2025-09-14 14:18:05 +0300

    Add Header

---
+++ src/Pages/HomePage.tsx
@@ -0,0 +1,17 @@
+import FlashSales from "@/components/HomePage/FlashSales";
+import Header from "@/components/HomePage/Header";
+import Hero from "@/components/HomePage/Hero";
+import NavBar from "@/components/HomePage/NavBar";
+
+function HomePage() {
+  return (
+    <>
+      <Header />
+      <NavBar />
+      <Hero />
+      <FlashSales />
+    </>
+  );
+}
+
+export default HomePage;
---
+++ src/components/HomePage/Header.tsx
@@ -0,0 +1,41 @@
+function Header() {
+  return (
+    <div className="bg-black text-white relative flex items-center justify-center px-4 py-2 text-[7px] sm:text-sm md:text-lg">
+      {/* Centered Text */}
+      <div className="text-center">
+        <span>
+          Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+          <span className="underline ml-2 sm:font-bold cursor-pointer">
+            Shop Now
+          </span>
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
+// // //
+// //       {/* Right Dropdown */}
+// //       <div className="absolute right-0">
+// //         <DropdownMenu>
+// //           <DropdownMenuTrigger>
+// //             <div className="flex items-center cursor-pointer">
+// //               <span>English</span>
+// //               <RiArrowDropDownLine size={20} />
+// //             </div>
+// //           </DropdownMenuTrigger>
+// //           <DropdownMenuContent>
+// //             <DropdownMenuItem>Arabic</DropdownMenuItem>
+// //             <DropdownMenuItem>French</DropdownMenuItem>
+// //             <DropdownMenuItem>Spanish</DropdownMenuItem>
+// //           </DropdownMenuContent>
+// //         </DropdownMenu>
+// //       </div>
+// // import {
+// //   DropdownMenu,
+// //   DropdownMenuContent,
+// //   DropdownMenuItem,
+// //   DropdownMenuTrigger,
+// // } from "@/components/ui/dropdown-menu";
+// // import { RiArrowDropDownLine } from "react-icons/ri";
