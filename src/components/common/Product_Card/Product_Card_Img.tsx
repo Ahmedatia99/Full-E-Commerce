789f09422e2cacaf658feb3d0e2584a9bea71877
unknown <bamrmagdy@gmail.com>
2025-09-17 22:39:15 +0300

    Edit Cart Component To Be Active With The Cart Context

--- src/components/common/Product_Card/Product_Card_Img.tsx
+++ src/components/common/Product_Card/Product_Card_Img.tsx
@@ -1,3 +1,4 @@
+import { memo } from "react";
 import AddToCartButton from "./Prodct_Card_Add_To_Cart_Btn";
 
 type ProductImageProps = {
@@ -7,26 +8,24 @@ type ProductImageProps = {
 };
 
 const ProductImage = ({ src, alt, itemProp }: ProductImageProps) => (
-  <>
-    <div className=" px-10 py-7 flex justify-center mt-4 ">
-      <img
-        src={`${src}?f_auto&fit=clip&w=190&h=138&q=auto:good`}
-        srcSet={`
+  <div className="px-10 py-7 flex justify-center mt-4">
+    <img
+      src={`${src}?f_auto&fit=clip&w=190&h=138&q=auto:good`}
+      srcSet={`
         ${src}?fm=webp&fit=clip&w=380&h=276&q=auto:good 2x,
         ${src}?fm=webp&fit=clip&w=570&h=414&q=auto:good 3x
       `}
-        alt={alt}
-        fetchPriority="high"
-        itemProp={itemProp}
-        width={190}
-        height={138}
-        loading="eager"
-        decoding="async"
-        style={{ maxWidth: "100%", height: "auto" }}
-        className="transition-transform duration-300 hover:scale-105"
-      />
-    </div>
-  </>
+      alt={alt}
+      fetchPriority="high"
+      itemProp={itemProp}
+      width={190}
+      height={138}
+      loading="eager"
+      decoding="async"
+      style={{ maxWidth: "100%", height: "auto" }}
+      className="transition-transform duration-300 hover:scale-105"
+    />
+  </div>
 );
 
-export default ProductImage;
+export default memo(ProductImage);
