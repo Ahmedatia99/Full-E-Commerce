3b6718732f5e51d6077f943ee87cf2bcada69292
unknown <bamrmagdy@gmail.com>
2025-09-17 16:28:19 +0300

    Enhance Responsivity for 404Page

--- src/Pages/NotFound404Page.tsx
+++ src/Pages/NotFound404Page.tsx
@@ -1,3 +1,4 @@
+import Breadcrumbs from "@/components/common/Breadcrumbs";
 import { Button } from "@/components/ui/button";
 import React from "react";
 import { Link } from "react-router-dom";
@@ -5,13 +6,21 @@ import { Link } from "react-router-dom";
 function NotFound404Page() {
   return (
     <>
-      <div className="flex items-center justify-center h-screen flex-col">
-        <h1 className="font-bold text-[70px]">404 Not Found</h1>
-        <p>Your visited page not found. You may go home page.</p>
-        <Button className="py-7 mt-18">
-          {" "}
-          <Link to="/">Back to home page</Link>{" "}
-        </Button>
+      <div className="container mx-auto px-5">
+        <Breadcrumbs />
+
+        <div className="flex items-center justify-center  flex-col text-center mt-20">
+          <h1 className="font-bold text-[30px] md:text-[70px]">
+            404 Not Found
+          </h1>
+          <p>Your visited page not found. You may go home page.</p>
+          <Button className="py-7 mt-18 mb-10">
+            {" "}
+            <Link className="" to="/">
+              Back to home page
+            </Link>{" "}
+          </Button>
+        </div>
       </div>
     </>
   );
