ecd3e887658fc2c92117d624a966be542ac0f821
Taha Ebrahim <tahsniper312@gmail.com>
2025-09-23 19:15:09 +0300

    added Language and fixed responsive and btn

--- src/components/HomePage/Browse-By-Category-Section/Categories.tsx
+++ src/components/HomePage/Browse-By-Category-Section/Categories.tsx
@@ -6,80 +6,92 @@ import { LaptopMinimal, Camera, Headphones, Gamepad } from "lucide-react";
 import { IoWatchOutline } from "react-icons/io5";
 import { AiOutlineMobile } from "react-icons/ai";
 import { Swiper as SwiperType } from "swiper";
+import { useTranslation } from "react-i18next";
+import i18n from "@/i18n";
 
 function Categories() {
+  const { t } = useTranslation();
+
   const swiperRef = useRef<SwiperType | null>(null);
   const categories = [
     {
       id: 1,
-      name: "phones",
+      name: t("Phones"),
       icon: <AiOutlineMobile className="category-icon" />,
       navigate: "/phone",
     },
     {
       id: 2,
-      name: "phones",
+      name: t("Laptops"),
       icon: <LaptopMinimal className="category-icon" />,
-      navigate: "/phone",
+      navigate: "/laptop",
     },
     {
       id: 3,
-      name: "phones",
+      name: t("Watches"),
       icon: <IoWatchOutline className="category-icon" />,
-      navigate: "/phone",
+      navigate: "/watch",
     },
     {
       id: 4,
-      name: "phones",
+      name: t("Cameras"),
       icon: <Camera className="category-icon" />,
-      navigate: "/phone",
+      navigate: "/camera",
     },
     {
       id: 5,
-      name: "phones",
+      name: t("Headphones"),
       icon: <Headphones className="category-icon" />,
-      navigate: "/phone",
+      navigate: "/headphones",
     },
     {
       id: 6,
-      name: "phones",
+      name: t("Gaming"),
       icon: <Gamepad className="category-icon" />,
-      navigate: "/phone",
+      navigate: "/gaming",
     },
     {
       id: 7,
-      name: "phones",
+      name: t("Accessories"),
       icon: <Camera className="category-icon" />,
-      navigate: "/phone",
+      navigate: "/accessories",
     },
   ];
+
   return (
     <section className="px-2">
-      <div className=" mt-10 flex  justify-between w-full mb-5 md:mb-10">
+      <div className="mt-10 flex justify-between w-full mb-5 md:mb-10">
         <SectionHeader
-          label="Categories"
-          title="Browse By Category"
+          label={t("Categories")}
+          title={t("Browse By Category")}
           swiperRef={swiperRef}
         />
       </div>
+
       <Swiper
+        key={i18n.dir()}
+        dir={i18n.dir()}
+        
         modules={[Navigation]}
         onSwiper={(swiper) => (swiperRef.current = swiper)}
-        spaceBetween={20}
-        slidesPerView={2}
+        spaceBetween={16}
+        slidesPerView={2} // 👈 الأساس للموبايل
         breakpoints={{
-          640: { slidesPerView: 3 },
-          1024: { slidesPerView: 4 },
-          1280: { slidesPerView: 6 },
+          480: { slidesPerView: 3, spaceBetween: 16 }, // موبايل كبير
+          640: { slidesPerView: 4, spaceBetween: 20 }, // تابلت صغير
+          1024: { slidesPerView: 5, spaceBetween: 24 }, // لابتوب
+          1280: { slidesPerView: 6, spaceBetween: 28 }, // ديسكتوب كبير
         }}
       >
         {categories.map((category) => (
           <SwiperSlide key={category.id}>
-            <div className="w-fit flex flex-col items-center justify-center border py-5 lg:py-10 px-10 lg:px-20 rounded-lg hover:text-white hover:bg-main cursor-pointer">
-              <div className="rounded-md flex items-center justify-center">
+            <div className="flex flex-col items-center justify-center border py-6 sm:py-8 md:py-10 px-6 sm:px-8 md:px-12 lg:px-16 rounded-lg hover:text-white hover:bg-main cursor-pointer transition">
+              <div className="rounded-md flex items-center justify-center text-3xl sm:text-4xl md:text-5xl mb-2">
                 {category.icon}
               </div>
-              <span className="font-semibold">{category.name}</span>
+              <span className="font-medium text-sm sm:text-base md:text-lg">
+                {category.name}
+              </span>
             </div>
           </SwiperSlide>
         ))}
@@ -87,4 +99,5 @@ function Categories() {
     </section>
   );
 }
+
 export default Categories;
--- src/components/HomePage/Featured-Products-Section/Featured.tsx
+++ src/components/HomePage/Featured-Products-Section/Featured.tsx
@@ -4,16 +4,18 @@ import woman from "@/assets/images/fashion.png";
 import ps5 from "@/assets/images/ps5.png";
 import speakers from "@/assets/images/speakers.png";
 import perfume from "@/assets/images/perfume.png";
+import { useTranslation } from "react-i18next";
 function Featured() {
+  const { t } = useTranslation();
   return (
     <section className="my-20">
       <SectionHeader
-        label="featured"
-        title="Featured Products"
+        label={t("featured")}
+        title={t("Featured Products")}
         className="mb-20"
       />
 
-      <div className="flex max-sm:flex-col md:flex-row justify-center gap-2 md:gap-5 lg:gap-10 ">
+      <div className="grid grid-cols-1  md:grid-cols-2 gap-5 md:gap-8 ">
         {/* Left big card */}
         <CategoryCard
           title="PlayStation 5"
@@ -23,14 +25,14 @@ function Featured() {
         />
 
         {/* Right side grid */}
-        <div className="flex flex-col">
+        <div className="grid grid-rows-2 gap-5 md:gap-8">
           <CategoryCard
             title="Women’s Collections"
             description="Featured woman collections that give you another vibe."
             image={woman}
           />
 
-          <div className="flex mt-4 gap-4 items-center justify-center">
+          <div className="grid grid-cols-2 max-[400px]:grid-cols-1 gap-5 md:gap-8">
             <CategoryCard
               title="Speakers"
               description="Amazon wireless speakers"
